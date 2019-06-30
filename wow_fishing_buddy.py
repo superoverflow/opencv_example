19aa742cd6e9a2a0d5306c6045ba8a4d607e40ea
cyrus.tang <cyrus.tang@revolut.com>
2019-06-30 23:28:31 +0100

    fix indent

--- wow_fishing_buddy.py
+++ wow_fishing_buddy.py
@@ -44,9 +44,9 @@ def compute_diff_from_images(img1, img2):
 
     diff = (diff * 255).astype("uint8")
     thresh = cv2.threshold(diff, 0, 255,
-    	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     im, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	    cv2.CHAIN_APPROX_SIMPLE)
+        cv2.CHAIN_APPROX_SIMPLE)
     contours.sort(key=cv2.contourArea, reverse=True)
     biggest_cnt = contours[0]
     return biggest_cnt
@@ -62,48 +62,48 @@ def find_center_of_fishing_float(focus_region,cv2_cnt):
     return cX, cY
 
 def listen():
-	logging.debug('Well, now we are listening for loud sounds...')
-	CHUNK = 1024  # CHUNKS of bytes to read each time from mic
-	FORMAT = pyaudio.paInt16
-	CHANNELS = 1
-	RATE = 18000
-	THRESHOLD = 1600  # The threshold intensity that defines silence
-	                  # and noise signal (an int. lower than THRESHOLD is silence).
-	SILENCE_LIMIT = 1  # Silence limit in seconds. The max ammount of seconds where
-	                   # only silence is recorded. When this time passes the
-	                   # recording finishes and the file is delivered.
-	p = pyaudio.PyAudio()
-	stream = p.open(format=FORMAT,
-	                channels=CHANNELS,
-	                rate=RATE,
-	                input=True,
-	                frames_per_buffer=CHUNK)
-	cur_data = ''  # current chunk  of audio data
-	rel = int(RATE/CHUNK)
-	slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+    logging.debug('Well, now we are listening for loud sounds...')
+    CHUNK = 1024  # CHUNKS of bytes to read each time from mic
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 18000
+    THRESHOLD = 1600  # The threshold intensity that defines silence
+                      # and noise signal (an int. lower than THRESHOLD is silence).
+    SILENCE_LIMIT = 1  # Silence limit in seconds. The max ammount of seconds where
+                       # only silence is recorded. When this time passes the
+                       # recording finishes and the file is delivered.
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    cur_data = ''  # current chunk  of audio data
+    rel = int(RATE/CHUNK)
+    slid_win = deque(maxlen=SILENCE_LIMIT * rel)
 
-	success = False
-	listening_start_time = time.time()
-	while True:
-		try:
-			cur_data = stream.read(CHUNK)
-			slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
-			sound = [x > THRESHOLD for x in slid_win]
-			if max(slid_win) > 1500:
-			   logging.debug("sound level  [{}]".format(max(slid_win)))
-			if(sum(sound) > 0):
-				logging.debug('I heard something!')
-				success = True
-				break
-			if time.time() - listening_start_time > 20:
-				logging.debug('I dont hear anything already 20 seconds!')
-				break
-		except IOError:
-			break
+    success = False
+    listening_start_time = time.time()
+    while True:
+        try:
+            cur_data = stream.read(CHUNK)
+            slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
+            sound = [x > THRESHOLD for x in slid_win]
+            if max(slid_win) > 1500:
+               logging.debug("sound level  [{}]".format(max(slid_win)))
+            if(sum(sound) > 0):
+                logging.debug('I heard something!')
+                success = True
+                break
+            if time.time() - listening_start_time > 20:
+                logging.debug('I dont hear anything already 20 seconds!')
+                break
+        except IOError:
+            break
 
-	stream.close()
-	p.terminate()
-	return success
+    stream.close()
+    p.terminate()
+    return success
 
 def send_fishing_float():
     pyautogui.press("1")
@@ -129,6 +129,10 @@ def main():
     img2 = get_screenshot(region=region)
     biggest_cnt = compute_diff_from_images(img1, img2)
     cur_x, cur_y = find_center_of_fishing_float(focus_region=region, cv2_cnt=biggest_cnt)
+    biggest_cnt_shifted = biggest_cnt + (region[0], region[1])
+    M = cv2.moments(biggest_cnt_shifted)
+    cur_x = int(M["m10"] / M["m00"])
+    cur_y = int(M["m01"] / M["m00"])
     logging.info("Float center at: {}, {}".format(cur_x, cur_y))
 
     move_cursor_to_fishing_float(cur_x, cur_y, 0.5)
