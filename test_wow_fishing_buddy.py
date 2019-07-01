8a6e156098facc42e9ffa9450149867f4f1a160c
cyrus.tang <cyrus.tang@revolut.com>
2019-07-01 18:02:55 +0100

    modify test

--- test_wow_fishing_buddy.py
+++ test_wow_fishing_buddy.py
@@ -1,10 +1,13 @@
 from wow_fishing_buddy import compute_diff_from_images
 import cv2
+from skimage.measure import compare_ssim
 
 def test_compute_diff_from_images():
-    img1 = cv2.imread("text_fixtures/image1.png")
-    img2 = cv2.imread("text_fixtures/image2.png")
-    cv2.imshow("img1", img1)
-    cv2.waitKey(0)
+    img1 = cv2.imread("test_fixtures/image1.png")
+    img2 = cv2.imread("test_fixtures/image2.png")
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    (score, diff) = compare_ssim(gray1, gray2, full=True)
+
 
 test_compute_diff_from_images()
