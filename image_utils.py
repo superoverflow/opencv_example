c93be6f421ee38ffee4564b819ef57e6e74de11b
cyrus.tang <cyrus.tang@revolut.com>
2019-07-02 23:06:17 +0100

    add test fixtures

---
+++ image_utils.py
@@ -0,0 +1,12 @@
+import cv2
+
+def diff_images(img1, img2):
+    """ derives the diff between img1, img2
+    """
+    pass
+
+def get_contours(img):
+    pass
+
+def get_centroid(contour):
+    pass
