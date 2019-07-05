0340d3e2b43ef68d1d4885053fa2b9d01f5cb530
cyrus.tang <cyrus.tang@revolut.com>
2019-07-05 17:15:31 +0100

    update testing

--- test_image_utils.py
+++ test_image_utils.py
@@ -4,21 +4,16 @@ import numpy as np
 from snapshottest.file import FileSnapshot
 
 def test_diff_images(snapshot, tmpdir):
-    img1 = cv2.imread('test_fixtures/background00.jpg')
-    img2 = cv2.imread('test_fixtures/float00.jpg')
-    result = diff_images(img1, img2)
+    before = cv2.imread('test_fixtures/background00.jpg')
+    after = cv2.imread('test_fixtures/float00.jpg')
+    diff = diff_images(before, after)
+    contour = get_contours(diff)
+    result = after.copy()
+    cv2.drawContours(result, [contour], 0, (0, 255, 0))
     tmpfile = tmpdir.join('test_diff_images_result.bmp')
     cv2.imwrite(str(tmpfile), result)
     snapshot.assert_match(FileSnapshot(str(tmpfile)))
 
-# def test_get_contours():
-#     img = cv2.imread('test_fixtures/test_diff_images_result.bmp',
-#                     cv2.IMREAD_GRAYSCALE)
-#     bg = cv2.imread('test_fixtures/float00.jpg')
-#     contour = get_contours(img)
-#     cv2.drawContours(bg, [contour], 0, (0, 255, 255))
-#     cv2.imshow("contour-with-bg", bg)
-#     cv2.waitKey(0)
 
 
 
